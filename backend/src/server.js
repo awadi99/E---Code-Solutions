70b93e46cb1326283798e965201b2c221c275389
awadi99 <adityawaghmare9990@gmail.com>
2026-09-18 10:48:13 +0530

    Update Backend

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import { connectionDb } from "./config/db.js";
+import express from 'express'
+import dotenv from 'dotenv';
+import app from "./app.js";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+await connectionDb();
+
+const server = app.listen(PORT,()=>{
+  console.log(`server running on ${PORT}`);
+})
